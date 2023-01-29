c90ffd6c198f6f4f21eb79d627941bb6e65b0189
unknown <jocelynlemer@yahoo.com>
2023-01-29 15:49:25 -0600

    Created the Functionality to Update a Specific Product

--- products/views.py
+++ products/views.py
@@ -6,7 +6,8 @@ from .serializers import ProductSerializer
 from .models import Product
 
 @api_view(['GET', 'POST'])
-def products_list(request):
+def product_list(request):
+    
     if request.method == 'GET':
         products= Product.objects.all()
         serializer= ProductSerializer(products, many=True)
@@ -16,4 +17,19 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+       serializer=ProductSerializer(product, data= request.data)
+       serializer.is_valid(raise_exception= True)
+       serializer.save()
+       return Response(serializer.data)
+
+   
 
